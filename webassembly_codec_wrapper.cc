f397a9ced671e5104c6d602fd36b16141d875591
Michael Yitayew <michael@videoinfra.dev>
2022-11-04 16:57:46 -0400

    Removed tensorflow directory and added it as a git repository dependency. Also exposed encodeAudioBuffer and decodeAudioBuffer functions through wasm.

--- webassembly_codec_wrapper.cc
+++ webassembly_codec_wrapper.cc
@@ -191,6 +191,97 @@ void CreateDecoder() {
   }
 }
 
+uint64_t EncodeBuffer(uintptr_t data, uint64_t num_samples, uint32_t sample_rate_hz, uint8_t* output) {
+  // Select encoder to use.
+  chromemedia::codec::LyraEncoder* encoder_to_use = nullptr;
+  switch (sample_rate_hz) {
+    case 8000:
+      encoder_to_use = encoder_8khz.get();
+      break;
+    case 16000:
+      encoder_to_use = encoder_16khz.get();
+      break;
+    case 32000:
+      encoder_to_use = encoder_32khz.get();
+      break;
+    case 48000:
+      encoder_to_use = encoder.get();
+      break;
+    default: {
+      fprintf(stderr, "Unsupported sample rate: %d", sample_rate_hz);
+      return -1;
+    }
+  }
+  // Convert the float input data to int16_t.
+  float* data_ptr = reinterpret_cast<float*>(data);
+  std::vector<int16_t> data_to_encode(num_samples);
+  for (int i = 0; i < num_samples; i++) {
+    data_to_encode[i] = static_cast<int16_t>(data_ptr[i] * 32767.0f);
+  }
+
+  // Encode the input audio.
+  auto maybe_encoded_data = chromemedia::codec::EncodeWithEncoder(
+      encoder_to_use, data_to_encode, sample_rate_hz);
+  if (!maybe_encoded_data.has_value()) {
+    fprintf(stderr, "Failed to encode audio.\n");
+    return -1;
+  }
+
+  // Copy the encoded data to the output buffer.
+  const std::vector<uint8_t>& encoded_data = maybe_encoded_data.value();
+  const uint64_t num_encoded_samples = encoded_data.size();
+  uint8_t* output_ptr = reinterpret_cast<uint8_t*>(output);
+  for (int i = 0; i < num_encoded_samples; i++) {
+    output_ptr[i] = encoded_data[i];
+  }
+
+  return num_encoded_samples;
+}
+
+uint64_t DecodeBuffer(uint8_t *data, uint64_t num_samples, uint32_t sample_rate_hz, uintptr_t output) {
+  // Select decoder to use.
+  chromemedia::codec::LyraDecoder* decoder_to_use = nullptr;
+  switch (sample_rate_hz) {
+    case 8000:
+      decoder_to_use = decoder_8khz.get();
+      break;
+    case 16000:
+      decoder_to_use = decoder_16khz.get();
+      break;
+    case 32000:
+      decoder_to_use = decoder_32khz.get();
+      break;
+    case 48000:
+      decoder_to_use = decoder.get();
+      break;
+    default: {
+      fprintf(stderr, "Unsupported sample rate: %d", sample_rate_hz);
+      return -1;
+    }
+  }
+  std::vector<uint8_t> data_to_decode(num_samples);
+  for (int i = 0; i < num_samples; i++) {
+    data_to_decode[i] = *reinterpret_cast<uint8_t*>(data + i);
+  }
+  auto maybe_decoded_output = chromemedia::codec::DecodeWithDecoder(
+      decoder_to_use, data_to_decode, /*packet_loss_rate=*/0.f,
+      /*float_average_burst_length=*/1.f);
+  if (!maybe_decoded_output.has_value()) {
+    fprintf(stderr, "Failed to decode audio.\n");
+    return -1;
+  }
+
+  // Copy the decoded data to the output buffer.
+  const std::vector<int16_t>& decoded_output = maybe_decoded_output.value();
+  const int num_decoded_samples = decoded_output.size();
+  float* output_ptr = reinterpret_cast<float*>(output);
+  for (int i = 0; i < num_decoded_samples; i++) {
+    output_ptr[i] = static_cast<float>(decoded_output[i] / 32767.0f);
+  }
+
+  return num_decoded_samples;
+}
+
 bool EncodeAndDecodeWithLyra(uintptr_t data, uint32_t num_samples,
                              uint32_t sample_rate_hz, uintptr_t out_data) {
   fprintf(stdout, "EncodeAndDecode called with %d samples.\n", num_samples);
@@ -265,4 +356,6 @@ EMSCRIPTEN_BINDINGS(module) {
   emscripten::function("isCodecReady", IsCodecReady);
   emscripten::function("encodeAndDecode", EncodeAndDecodeWithLyra,
                        emscripten::allow_raw_pointers());
+  emscripten::function("encodeAudioBuffer", EncodeBuffer, emscripten::allow_raw_pointers());
+  emscripten::function("decodeAudioBuffer", DecodeBuffer, emscripten::allow_raw_pointers());
 }
